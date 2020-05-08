f3a478fcd6cfefe6a772bad227c11d8edf14e3a0
mattuu <maciej@uliasz.com>
2020-05-08 21:18:43 +0100

    Updated CI def

--- .circleci/codecov.ps1
+++ .circleci/codecov.ps1
@@ -4,7 +4,7 @@ param
 )
 $ver = (gci "$env:userprofile\.nuget\packages\codecov").Name
 $cmd = "$env:userprofile\.nuget\packages\codecov\$ver\tools\codecov.exe";
-$fName = ".\TestResults\coverage.opencover.xml";
+$fName = "..\TestResults\coverage.opencover.xml";
 $arg1 = "-f ""$fName""";
 $arg2 = "-t $token";
 & $cmd $arg1 $arg2

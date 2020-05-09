1015593668108c73407379e34d5d73cc90077c3f
mattuu <maciej@uliasz.com>
2020-05-09 10:56:30 +0100

    Updated ps1

--- .circleci/codecov.ps1
+++ .circleci/codecov.ps1
@@ -6,6 +6,6 @@ param
 
 $ver = (gci "$env:userprofile\.nuget\packages\codecov").Name
 $cmd = "$env:userprofile\.nuget\packages\codecov\$ver\tools\codecov.exe";
-$arg1 = "-f ""$fName""";
+$arg1 = "-f $fName";
 $arg2 = "-t $token";
 & $cmd $arg1 $arg2
